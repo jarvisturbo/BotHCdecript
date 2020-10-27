d66a19ac7c9f96e7f6dffd3cead8904db166558b
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2020-10-27 15:27:10 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 dirb="/etc/BotHCdecript" && [[ ! -d ${dirb} ]] && mkdir ${dirb}
 dirs="${dirb}/sources" && [[ ! -d ${dirs} ]] && mkdir ${dirs}
-SCPresq="aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3J1ZGk5OTk5L1ZQU0JvdC9tYWluL3NvdXJjZXM="
+SCPresq="aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3J1ZGk5OTk5L0JvdEhDZGVjcmlwdC9tYWluL3NvdXJjZXM="
 SUB_DOM='base64 -d'
 bar="\e[0;36m=====================================================\e[0m"
 
